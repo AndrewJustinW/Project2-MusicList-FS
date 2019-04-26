17249c2ba2834f8ff461565919d95c0bda6c7c85
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-26 10:48:54 -0400

    added file structure

--- db/connection.js
+++ db/connection.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose')
 
 
 // Connect to a local database called "the-donut-shop"
-mongoose.connect("mongodb://localhost/Music-List", {useNewUrlParser:true}).then(()=>{
+mongoose.connect("mongodb://localhost/Project-2", {useNewUrlParser:true}).then(()=>{
     console.log ("Connected to MongoDB")
 })
 // When it connects, then console.log "Connected to MongoDB"
+
+const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project-2'
+mongoose.connect(dbConnection, { useNewUrlParser: true})
+  .then(() => {
+    console.log("mongo is working");
+  })
 
 
 // export your mongoose connection
