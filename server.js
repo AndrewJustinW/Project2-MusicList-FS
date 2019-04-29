fbf7b350757df57b99b788565a45c2c1bae0abe8
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-29 11:06:33 -0400

    added a variable that leads to routes directory

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express')
 
-const app = express();
+const app = express()
+
+const methodOveride = require('method-override')
+const routes = require('./routes/index')
+
+
+app.use(express.urlencoded())
+
+app.use(methodOveride('_method'))
+
+app.set('view engine', 'hbs');
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`we are now connected to ${PORT}`)
-})
-
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`)
+});
