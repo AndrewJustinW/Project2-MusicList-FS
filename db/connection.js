cc6b60d7dc6f7a45a9933523dad97aa2d988a4a7
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-29 11:14:49 -0400

    added code to connection file in db folder

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,6 @@
 const mongoose = require('mongoose')
 
 
-// Connect to a local database called "the-donut-shop"
-mongoose.connect("mongodb://localhost/Project-2", {useNewUrlParser:true}).then(()=>{
-    console.log ("Connected to MongoDB")
-})
 // When it connects, then console.log "Connected to MongoDB"
 
 const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project-2'
