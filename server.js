8ab3755cdc3bbd7c99716aaef5506aafd7d51989
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-29 11:09:55 -0400

    made changes to server.js including adding comments

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+
+// require express, method-override, and our routes
+
 const express = require('express')
-
 const app = express()
-
 const methodOveride = require('method-override')
 const routes = require('./routes/index')
 
-
+//= =====================
+// MIDDLEWARE
+//= =====================
 app.use(express.urlencoded())
-
+app.use(express.json())
 app.use(methodOveride('_method'))
-
 app.set('view engine', 'hbs');
 
+
+//= =====================
+// LISTENERS
+//= =====================
+// CONNECT MONGOOSE TO "musiclist"
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`)
