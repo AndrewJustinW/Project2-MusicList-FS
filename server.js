c687d4da7d46d85d221b5610c3c9416d1bed9790
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-30 14:23:48 -0400

    added ways to access APIS

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express')
 const app = express()
 const methodOveride = require('method-override')
 const routes = require('./routes/index')
+const artistAPI = require("./controllers/artistsAPI")
+const songAPI = require("./controllers/songAPI")
+const userAPI = require("./controllers/userAPI")
 
 //= =====================
 // MIDDLEWARE
@@ -16,9 +19,15 @@ app.set('view engine', 'hbs');
 
 
 //= =====================
+// Router
+//= =====================
+// Links the server to our Router File
+
+
+//= =====================
 // LISTENERS
 //= =====================
-// CONNECT MONGOOSE TO "musiclist"
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`)
