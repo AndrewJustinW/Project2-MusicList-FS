2a6b6da927c9b376873554a1b57635d934c5f947
AndrewJustinW <andrew.justinw@gmail.com>
2019-05-01 10:41:46 -0400

    changed stuff around

--- routes/Index.js
+++ routes/Index.js
@@ -4,6 +4,7 @@ let router = express.Router();
 const users = require("../controllers/userAPI");
 const artists = require("../controllers/artistsAPI");
 
+// Users API
 router.get("/", users.home);
 router.get("/user", users.index);
 router.get("/user/new", users.new);
@@ -13,5 +14,14 @@ router.get("/user/:uid/edit", users.edit);
 router.put("/user/:uid", users.update);
 router.delete("/user/:uid", users.delete);
 
+// for artist API
+
+// router.get("/user/:uid/artist", artists.index);
+// router.get("/user/:uid/artist/new/", users.new);
+// router.post("/artist", users.create);
+// router.get("/user/:uid/", users.show);
+// router.get("/user/:uid/edit", users.edit);
+// router.put("/user/:uid", users.update);
+// router.delete("/user/:uid", users.delete);
 
 module.exports = router;
