3f458d6c394bb754553837776c991e8adae831ae
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-30 21:32:17 -0400

    changed routes.js

--- routes/Index.js
+++ routes/Index.js
@@ -1,3 +1,17 @@
-let express = require('express')
-let router = express.Router()
-const musicListController = require('../controllers/musiclistAPI.js')
+let express = require('express');
+let router = express.Router();
+
+const users = require("../controllers/userAPI");
+const artists = require("../controllers/artistsAPI");
+
+router.get("/", users.home);
+router.get("/user", users.index);
+router.get("/user/new", users.new);
+router.post("/user", users.create);
+router.get("/user/:uid", users.show);
+router.get("/user/:uid/edit", users.edit);
+router.put("/user/:uid", users.update);
+router.delete("/user/:uid", users.delete);
+
+
+module.exports = router;
