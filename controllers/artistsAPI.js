2aebe1142b56a2178ac36556a77f0b3cf7b220c7
AndrewJustinW <andrew.justinw@gmail.com>
2019-05-01 11:32:22 -0400

    editting the artistAPI

--- controllers/artistsAPI.js
+++ controllers/artistsAPI.js
@@ -1,38 +1,61 @@
-const mongoose = require('./connection.js');
-const ObjectId = mongoose.Schema.Types.ObjectId
+const mongoose = require('../controllers/connection');
 
 let artistSchema = mongoose.Schema({
-    ArtistName: String,
-    Genre: String,
-    userId: ObjectId
+  ArtistName: String,
+  Genre: String,
+  userId: ObjectId
 });
 
 let artistCollection = mongoose.model('Artist', artistSchema);
 
-function createArtist(newUser) {
-  return artistCollection.create(newUser);
-}
+const artistController = {
 
-function getArtists() {
-  return artistCollection.find();
+  index: function (req, res) {
+    artistCollection.find()
+      .then(users => {
+        res.render("user/index", {
+          users
+        });
+      });
+  },
+  new: function (req, res) {
+    res.render("user/new");
+  },
+  create: function (req, res) {
+    artistCollection.create(req.body)
+      .then(() => {
+        res.redirect("/user");
+      });
+  },
+  show: function (req, res) {
+    artistCollection.findById(req.params.uid)
+      .then(user => {
+        res.render("user/show", {
+          user
+        });
+      });
+  },
+  edit: function (req, res) {
+    artistCollection.findById(req.params.uid)
+      .then(user => {
+        res.render("user/edit", {
+          user
+        });
+      });
+  },
+  update: function (req, res) {
+    artistCollection.findByIdAndUpdate(req.params.uid, req.body, {
+        new: true
+      })
+      .then(() => {
+        res.redirect("/user/" + req.params.uid);
+      });
+  },
+  delete: function (req, res) {
+    artistCollection.findByIdAndRemove(req.params.uid)
+      .then(() => {
+        res.redirect("/user/");
+      });
+  }
 }
-
-function getArtist(userId) {
-  return artistCollection.findById(userId);
-}
-
-function deleteArtist(userId) {
-  return artistCollection.deleteOne({ _id: userId});
-}
-
-function deleteAllArtists() {
-  return artistsCollection.deleteMany({});
-}
-
-module.exports = {
-  createArtist,
-  getArtists,
-  getArtist,
-  deleteArtist,
-  deleteAllArtists
-}
+module.exports = artistController;
