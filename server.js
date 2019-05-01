ac6e13836816f1ef802ed07bfb0fbea2769202b4
AndrewJustinW <andrew.justinw@gmail.com>
2019-04-30 21:33:42 -0400

    changed server.js

--- server.js
+++ server.js
@@ -3,30 +3,32 @@
 
 const express = require('express')
 const app = express()
-const methodOveride = require('method-override')
+const methodOverride = require('method-override')
 const routes = require('./routes/index')
-const artistAPI = require("./controllers/artistsAPI")
-const songAPI = require("./controllers/songAPI")
-const userAPI = require("./controllers/userAPI")
+// const artistAPI = require("./controllers/artistsAPI")
+// const songAPI = require("./controllers/songAPI")
+
+//...for decoding body as js object
+
+//...for "faking" DELETE and PUT/PATCH requests
 
 //= =====================
 // MIDDLEWARE
 //= =====================
 app.use(express.urlencoded())
 app.use(express.json())
-app.use(methodOveride('_method'))
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname+"/public"))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded())
+
+
+
+//add middleware for handlebars here
 app.set('view engine', 'hbs');
+app.use("/", routes)
 
 
-//= =====================
-// Router
-//= =====================
-// Links the server to our Router File
-
-
-//= =====================
-// LISTENERS
-//= =====================
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
