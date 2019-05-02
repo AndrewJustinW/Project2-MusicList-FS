e57f7e3e48b18e4a12867730fefdac4007cc2741
AndrewJustinW <andrew.justinw@gmail.com>
2019-05-01 20:41:58 -0400

    edited routes and added to the artist view folders

--- controllers/artistsAPI.js
+++ controllers/artistsAPI.js
@@ -1,4 +1,5 @@
 const mongoose = require('../controllers/connection');
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 let artistSchema = mongoose.Schema({
   ArtistName: String,
@@ -11,50 +12,57 @@ let artistCollection = mongoose.model('Artist', artistSchema);
 const artistController = {
 
   index: function (req, res) {
-    artistCollection.find()
-      .then(users => {
-        res.render("user/index", {
-          users
-        });
+    artistCollection.find({ userId: req.params.uid})
+      .then(artists => {
+        res.render("artists/index", {artists: artists})
+
       });
-  },
+},
+
   new: function (req, res) {
-    res.render("user/new");
+    User.findById(req.params.uid)
+      .then(user => {
+        res.render("artists/new", {user: user});
+      })
+
   },
+
+
   create: function (req, res) {
-    artistCollection.create(req.body)
+      artistCollection.create({
+      ArtistName: req.body.ArtistName,
+      Genre: req.body.Genre,
+      userId: req.params.uid
+  })
       .then(() => {
-        res.redirect("/user");
+          res.redirect("artist");
       });
   },
+
+
+
   show: function (req, res) {
-    artistCollection.findById(req.params.uid)
-      .then(user => {
-        res.render("user/show", {
-          user
-        });
+    artistCollection.findById(req.params.aid)
+      .then(artist => {
+        res.render("artist/show", {artist: artist});
       });
   },
   edit: function (req, res) {
-    artistCollection.findById(req.params.uid)
-      .then(user => {
-        res.render("user/edit", {
-          user
-        });
+    artistCollection.findById(req.params.aid)
+      .then(artist => {
+        res.render("artist/edit", {artist: artist});
       });
   },
   update: function (req, res) {
-    artistCollection.findByIdAndUpdate(req.params.uid, req.body, {
-        new: true
-      })
+    artistCollection.findByIdAndUpdate(req.params.aid, req.body, {new: true})
       .then(() => {
-        res.redirect("/user/" + req.params.uid);
+        res.redirect("/artist/" + req.params.aid);
       });
   },
   delete: function (req, res) {
-    artistCollection.findByIdAndRemove(req.params.uid)
+    artistCollection.findByIdAndRemove(req.params.aid)
       .then(() => {
-        res.redirect("/user/");
+        res.redirect("/artist/");
       });
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 const users = require("../controllers/userAPI");
-// const artists = require("../controllers/artistsAPI");
+const artists = require("../controllers/artistsAPI");
 
 // Users API
 router.get("/", users.home);
@@ -16,13 +16,13 @@ router.delete("/user/:uid", users.delete);
 
 // for artist API
 
-// router.get("/user/:uid/artist", artists.index);
-// router.get("/user/:uid/artist/new/", users.new);
-// router.post("/artist", users.create);
-// router.get("/user/:uid/", users.show);
-// router.get("/user/:uid/edit", users.edit);
-// router.put("/user/:uid", users.update);
-// router.delete("/user/:uid", users.delete);
+router.get("/user/:uid/artist", artists.index);
+router.get("/user/:uid/artist/new/", artists.new);
+router.post("/user/:uid/artist", artists.create);
+router.get("/user/:uid/artist/:aid", artists.show);
+router.get("/user/:uid/artist/:aid/edit", artists.edit);
+router.put("/user/:uid/artist/:aid/", artists.update);
+router.delete("/user/:uid/artist/:aid", artists.delete);
 
 
 module.exports = router;
