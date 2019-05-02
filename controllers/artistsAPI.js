69669b10c5b139024556ad478c9f1cd18c162421
AndrewJustinW <andrew.justinw@gmail.com>
2019-05-01 22:28:04 -0400

    editting views and apis

--- controllers/artistsAPI.js
+++ controllers/artistsAPI.js
@@ -1,35 +1,28 @@
-const mongoose = require('../controllers/connection');
-const ObjectId = mongoose.Schema.Types.ObjectId
+const ArtistSchema = require("../models/artistSchema")
+const UserSchema = require("../models/userSchema")
 
-let artistSchema = mongoose.Schema({
-  ArtistName: String,
-  Genre: String,
-  userId: ObjectId
-});
-
-let artistCollection = mongoose.model('Artist', artistSchema);
 
 const artistController = {
 
   index: function (req, res) {
-    artistCollection.find({ userId: req.params.uid})
+    ArtistSchema.find({ userId: req.params.uid})
       .then(artists => {
-        res.render("artists/index", {artists: artists})
+        res.render("artist/index", {artists: artists})
 
       });
 },
 
   new: function (req, res) {
-    User.findById(req.params.uid)
+    UserSchema.findById(req.params.uid)
       .then(user => {
-        res.render("artists/new", {user: user});
+        res.render("artist/new", {user: user});
       })
 
   },
 
 
   create: function (req, res) {
-      artistCollection.create({
+      ArtistSchema.create({
       ArtistName: req.body.ArtistName,
       Genre: req.body.Genre,
       userId: req.params.uid
@@ -42,27 +35,27 @@ const artistController = {
 
 
   show: function (req, res) {
-    artistCollection.findById(req.params.aid)
+    ArtistSchema.findById(req.params.aid)
       .then(artist => {
         res.render("artist/show", {artist: artist});
       });
   },
   edit: function (req, res) {
-    artistCollection.findById(req.params.aid)
+    ArtistSchema.findById(req.params.aid)
       .then(artist => {
         res.render("artist/edit", {artist: artist});
       });
   },
   update: function (req, res) {
-    artistCollection.findByIdAndUpdate(req.params.aid, req.body, {new: true})
+    ArtistSchema.findByIdAndUpdate(req.params.aid, req.body, {new: true})
       .then(() => {
-        res.redirect("/artist/" + req.params.aid);
+        res.redirect("/user/" + req.params.uid + "/artist/" + req.params.aid);
       });
   },
   delete: function (req, res) {
-    artistCollection.findByIdAndRemove(req.params.aid)
+    ArtistSchema.findByIdAndRemove(req.params.aid)
       .then(() => {
-        res.redirect("/artist/");
+        res.redirect("/user/" + req.params.uid + "/artist/");
       });
   }
 }
--- controllers/userAPI.js
+++ controllers/userAPI.js
@@ -1,59 +1,61 @@
-const mongoose = require('../controllers/connection');
-
-let userSchema = mongoose.Schema({
-    Username: String,
-    FavoriteGenre: String,
-});
-
-let UserCollection = mongoose.model('User', userSchema);
+const UserSchema = require("../models/userSchema")
 
 const userController = {
     home: function (req, res) {
         res.render("home");
-      },
+    },
 
-  index: function (req, res) {
-    UserCollection.find()
-      .then(users => {
-        res.render("user/index", { users });
-      });
-  },
-  new: function (req, res) {
-    res.render("user/new");
-  },
-  create: function (req, res) {
-    UserCollection.create(req.body)
-      .then(() => {
-        res.redirect("/user");
-      });
-  },
-  show: function (req, res) {
-    UserCollection.findById(req.params.uid)
-      .then(user => {
-        res.render("user/show", { user });
-      });
-  },
-  edit: function (req, res) {
-    UserCollection.findById(req.params.uid)
-      .then(user => {
-        res.render("user/edit", { user });
-      });
-  },
-  update: function (req, res) {
-    UserCollection.findByIdAndUpdate(req.params.uid, req.body, { new: true })
-      .then(() => {
-        res.redirect("/user/" + req.params.uid);
-      });
-  },
-  delete: function (req, res) {
-    UserCollection.findByIdAndRemove(req.params.uid)
-      .then(() => {
-        res.redirect("/user/");
-      });
-  }
+    index: function (req, res) {
+        UserSchema.find()
+            .then(users => {
+                res.render("user/index", {
+                    users
+                });
+            });
+    },
+    new: function (req, res) {
+        res.render("user/new");
+    },
+    create: function (req, res) {
+        UserSchema.create(req.body)
+            .then(() => {
+                res.redirect("/user");
+            });
+    },
+    show: function (req, res) {
+        UserSchema.findById(req.params.uid)
+            .then(user => {
+                res.render("user/show", {
+                    user
+                });
+            });
+    },
+    edit: function (req, res) {
+        UserSchema.findById(req.params.uid)
+            .then(user => {
+                res.render("user/edit", {
+                    user
+                });
+            });
+    },
+    update: function (req, res) {
+        UserSchema.findByIdAndUpdate(req.params.uid, req.body, {
+                new: true
+            })
+            .then(() => {
+                res.redirect("/user/" + req.params.uid);
+            });
+    },
+    delete: function (req, res) {
+        UserSchema.findByIdAndRemove(req.params.uid)
+            .then(() => {
+                res.redirect("/user/");
+            });
+    }
 }
 //= =====================
 // EXPORTS
 //= =====================
 // export the controller with module.exports
-module.exports = userController;
+module.exports = userController
+    
